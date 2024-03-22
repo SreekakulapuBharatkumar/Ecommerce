c83467ea4ae44ed76425d832518ecbe9fe257604
Bharat <bharat.gmail.com>
2024-03-22 08:44:31 +0530

    Added User code

---
+++ e_commerce_user/src/main/java/com/feuji/ecommerce/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.feuji.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.feuji.ecommerce.dto.User;
+
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	User findByMailIdAndPassword(String mailId,String password);
+	
+	User findByUserId(int Userid);
+//	User findByUserId(int userid);
+}
